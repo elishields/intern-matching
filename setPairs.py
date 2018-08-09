30f1e63b4d460d3a3938630e746b5de35159e729
Eli Shields <elishields@gmail.com>
2018-08-08 21:21:49 -0700

    Imported files into PyCharm. Set System Interpreter (no virtual env). Added .idea/ to gitignore. Refactored functions from camelCase() to under_score() as in PEP8 standard.

--- setPairs.py
+++ setPairs.py
@@ -1,39 +1,43 @@
-from intern import Intern
-from utils import printHeader
-from setInterns import getChaosIntern
+from setInterns import get_chaos_intern
 
-def setPairs(session, interns):
+
+def set_pairs(session, interns):
     for round in range(session.num_rounds):
-        beginRoundUnmatched(round, interns)
-        while (unpairedExists(round, interns) is not None) and (unpairedExists(round, interns).name != "chaos"):
-            intern_to_pair = unpairedExists(round, interns)
-            ideal_match = findIdealMatch(round, intern_to_pair, interns)
+        begin_round_unmatched(round, interns)
+        while (unpaired_exists(round, interns) is not None) and (unpaired_exists(round, interns).name != "chaos"):
+            intern_to_pair = unpaired_exists(round, interns)
+            ideal_match = find_ideal_match(round, intern_to_pair, interns)
             if ideal_match == None:
-                ideal_match = getChaosIntern(interns)
+                ideal_match = get_chaos_intern(interns)
             rank = rankPair(intern_to_pair, ideal_match)
             propose(round, intern_to_pair, ideal_match, rank, interns)
 
-def beginRoundUnmatched(round, interns):
+
+def begin_round_unmatched(round, interns):
     for intern in interns:
         intern.pairs.update({ round: [None, 0, []] })
 
-def unpairedExists(round, interns):
+
+def unpaired_exists(round, interns):
     for intern in interns:
         if intern.pairs[round][0] is None:
             return intern
 
-def findIdealMatch(round, intern, interns):
+
+def find_ideal_match(round, intern, interns):
     for endorser_name, endorser_score in intern.endorsers.items():
-        if (not previousProposal(round, intern, endorser_name)) and (not previousMatch(round, intern, endorser_name)):
+        if (not previous_proposal(round, intern, endorser_name)) and (not previous_match(round, intern, endorser_name)):
             for look_for_intern in interns:
                 if look_for_intern.name == endorser_name:
                     return look_for_intern
     return None
 
-def previousProposal(round, intern, endorser):
+
+def previous_proposal(round, intern, endorser):
     return endorser in intern.pairs[round][2]
 
-def previousMatch(round, intern, endorser):
+
+def previous_match(round, intern, endorser):
     if intern.name == "chaos" or endorser == "chaos":
         return False
     for previous_round in range(round):
@@ -41,22 +45,26 @@ def previousMatch(round, intern, endorser):
             return True
     return False
 
+
 def propose(round, intern, match, rank, interns):
     intern.pairs[round][2].append(match.name)
-    if checkFree(round, match):
+    if check_free(round, match):
         pair(round, intern, match, rank)
     else:
-        if proposalAccepted(round, intern, match, rank):
+        if proposal_accepted(round, intern, match, rank):
             free(round, match, interns)
             pair(round, intern, match, rank)
 
-def checkFree(round, ideal_match):
+
+def check_free(round, ideal_match):
     return ideal_match.pairs[round][0] is None
+
 
 def rankPair(intern, match):
     for endorser_name, endorser_score in intern.endorsers.items():
         if endorser_name == match.name:
             return endorser_score
+
 
 def pair(round, intern, match, rank):
     intern.pairs[round][0] = match.name
@@ -64,9 +72,11 @@ def pair(round, intern, match, rank):
     match.pairs[round][0] = intern.name
     match.pairs[round][1] = rank
 
-def proposalAccepted(round, intern, match, proposed_rank):
+
+def proposal_accepted(round, intern, match, proposed_rank):
     current_rank = match.pairs[round][1]
     return proposed_rank >= current_rank
+
 
 def free(round, intern, interns):
     current_match = intern.pairs[round][0]
@@ -77,7 +87,8 @@ def free(round, intern, interns):
             intern.pairs[round][0] = None
             intern.pairs[round][1] = 0
 
-def getPairs(interns):
+
+def get_pairs(interns):
     for intern in interns:
         print("\n" + intern.name.upper())
         for key, value in intern.pairs.items():
--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 from tabulate import tabulate
 
-def printHeader(header):
+
+def print_header(header):
+
+
     border = ''
     for i in range(len(header) + 4):
         border += '-'
     title = '  ' + header + '  '
-    print ()
-    print (border)
-    print (title)
-    print (border)
+    print()
+    print(border)
+    print(title)
+    print(border)
 
-def printData(dataset):
-    print (tabulate(dataset, headers='keys', tablefmt='psql'))
+
+def print_data(dataset):
+    print(tabulate(dataset, headers='keys', tablefmt='psql'))
